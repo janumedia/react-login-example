7773014b103c7c49fe387cbc63a2416b189964af
janumedia <janumedia@gmail.com>
2022-02-06 11:24:14 +0800

    feat: auth routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import LoginForm from './LoginForm';
 import './App.css';
 import { AuthContext } from './contexts/AuthContext';
 import { useFetchData } from './api';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import { Dashboard } from './Dashboard';
+import { Home } from './Home';
 
 function App() {
   const [token, setToken] = useState<string | null>(null);
@@ -14,12 +15,29 @@ function App() {
     <div className="App">
       <AuthContext.Provider value={{token, setToken, isLoading, resultData, errorData, setErrorData, fecthData}}>
         <Routes>
+          <Route path='/' element={
+            <Requireuth>
+              <Home/>
+            </Requireuth>
+          }/>
           <Route path='/login' element={<LoginForm />}/>
-          <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/dashboard' element={
+            <Requireuth>
+              <Dashboard/>
+            </Requireuth>
+          }/>
         </Routes>
       </AuthContext.Provider>
     </div>
   );
+}
+
+function Requireuth({children} : {children:JSX.Element}) {
+  const location = useLocation();
+  const {token} = useContext(AuthContext);
+
+  if(token == null) return (<Navigate to='/login' state={{from: location}} replace/>);
+  return children;
 }
 
 export default App;
