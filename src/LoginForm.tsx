696e9f737102420b39b8536e270a361021301280
janumedia <janumedia@gmail.com>
2022-02-17 21:30:12 +0800

    feat: form validation using Formik and Yup

--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -1,18 +1,25 @@
-import React, { useContext, useEffect, useState } from "react";
+import { useFormik } from "formik";
+import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Alert, Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { Alert, Button, Form, FormFeedback, FormGroup, Input, Label } from "reactstrap";
 import { AuthContext } from "./contexts/AuthContext";
+import * as Yup from "yup";
 
 const LoginForm = () => {
   const {isLoading, login, token, user, getUser, errorData, setErrorData} = useContext(AuthContext);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  
-  const submit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    login(username, password)
-  };
+
+  const {handleSubmit, handleChange, handleBlur, values, touched, errors} = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    validationSchema: Yup.object({
+      username: Yup.string().min(3, 'Username required 3 chars min.').required('Username is required'),
+      password: Yup.string().min(3, 'Password required 3 chars min.').required('Password is required')
+    }),
+    onSubmit: values => login(values.username, values.password)
+  });
   
   useEffect(() => {
     if(token && !user) getUser(token);
@@ -26,36 +33,50 @@ const LoginForm = () => {
         token && !user
         ? 'Getting User...' 
         : (
-        <Form name="login-form" onSubmit={submit}>
+        <Form name="login-form" onSubmit={handleSubmit}>
           <FormGroup>
-            <Label>
+            <Label for="username">
               Username
               <Input
+                id="username"
+                name="username"
                 type="text"
                 placeholder="Your Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                value={values.username}
+                onChange={handleChange}
+                onBlur={handleBlur}
                 autoFocus
+                invalid={touched.username !== undefined && errors.username !== undefined}
               />
+              {
+                touched.username && errors.username && (<FormFeedback>{errors.username}</FormFeedback>)
+              }
             </Label>
             
           </FormGroup>
           <FormGroup>
-            <Label>
+            <Label for="password">
               Password
               <Input
+                id="password"
+                name="password"
                 type="password"
                 placeholder="Your Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                invalid={touched.password !== undefined && errors.password !== undefined}
               />
+              {
+                touched.password && errors.password && (<FormFeedback>{errors.password}</FormFeedback>)
+              }
             </Label>
           </FormGroup>
           <FormGroup>
             <Button
               type="submit"
               color="dark"
-              disabled={isLoading || (username.length < 3 || password.length < 3)}
+              disabled={isLoading}
             >
               { isLoading ? 'Signing In...' : 'Sign In' }
             </Button>
